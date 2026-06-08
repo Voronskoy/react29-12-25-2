3ef90d6d1b249084b19035942aff1e0fd05b0b8f
Markus_Voronskoy <marskoy27@gmail.com>
2026-06-08 11:31:17 +0300

    add formikStyles

--- src/components/FormikCamp/FormikCamp.jsx
+++ src/components/FormikCamp/FormikCamp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { campSchema } from "./../../validation/index";
+import styles from './FormikCamp.module.scss'
 
 const directionLabels = {
   "content-creator": "Контент-кріейтор",
@@ -19,11 +20,26 @@ const initialState = {
   age: "",
   email: "",
   direction: "content-creator",
-  experienc: "newbie",
+  experience: "newbie",
   about: "",
   isReady: false,
 };
 const FormikCamp = () => {
+  const showOption = ([key, value]) => (
+    <option key={key} value={key}>
+      {value}
+    </option>
+  );
+  const showRadio = ([key, value]) => (
+    <label key={key}>
+      <Field value={value} name="experience" type="radio" />
+      <span>{value}</span>
+    </label>
+  );
+  const handleSubmit = (values) => {
+    alert('submit')
+    console.log(values);
+  };
   return (
     <div>
       <div>
@@ -31,16 +47,64 @@ const FormikCamp = () => {
         <Formik
           initialValues={initialState}
           validationSchema={campSchema}
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           {() => {
             return (
-              <Form>
+              <Form className={styles.form}>
                 <label>
-                  <span>Ім'я або нікнейм</span>
-                  <Field type="text" placeholder="Наприклад, PixelFox" name='nickname'/>
-                  <ErrorMessage name='nickname'/>
+                  <span>
+                    Ім'я або нікнейм <span>*</span>
+                  </span>
+                  <Field
+                    type="text"
+                    placeholder="Наприклад, PixelFox"
+                    name="nickname"
+                  />
+                  <ErrorMessage name="nickname" />
                 </label>
+                <div className={styles.row}>
+                  <label>
+                    <span>Вік<span>*</span></span>
+                    <Field type="number" name="age" placeholder="12" />
+                    <ErrorMessage name="age" />
+                  </label>
+                  <label>
+                    <span>email<span>*</span></span>
+                    <Field
+                      type="email"
+                      name="email"
+                      placeholder="camp@example.com"
+                    />
+                    <ErrorMessage name="email" />
+                  </label>
+                </div>
+                <label>
+                  <span>Напрям табору<span>*</span></span>
+                  <Field as="select" name="derection">
+                    {Object.entries(directionLabels).map(showOption)}
+                  </Field>
+                </label>
+                <fieldset>
+                  <legend>Рівень досвіду<span>*</span></legend>
+                  <div>{Object.entries(experienceLabels).map(showRadio)}</div>
+                  <ErrorMessage name="experience" />
+                </fieldset>
+                <label>
+                  <span>
+                    Чому саме тебе треба взяти?<span>*</span>
+                  </span>
+                  <Field
+                    as="textarea"
+                    name="about"
+                    placeholder="Розкажи, що ти любиш створювати і який вайб принесеш у табір."
+                  />
+                </label>
+                <label>
+                  <Field type="checkbox" name="isReady" />
+                  <span>Готовий(-а) брати участь у мінічеленджах табору</span>
+                </label>
+                <button type="submit">Надіслати заявку</button>
               </Form>
             );
           }}
